cd68c2a22a79bbadfe32493cf5f48b3d6885576d
Hamza <hamzasarwari426@gmail.com>
2026-08-07 19:31:54 +0100

    Initial commit

---
+++ src/main.cpp
@@ -0,0 +1,17 @@
+#include <Arduino.h>
+#include <ESP32Servo.h>
+
+Servo myservo;
+const int servoPin = 13;
+
+void setup() {
+  myservo.attach(servoPin);
+  myservo.write(0);
+}
+  
+
+void loop() {
+
+  for(int i=0; i<=200; i++)
+}
+
