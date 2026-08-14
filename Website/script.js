afcd5f4629060bc932d97c20665b532d55710943
Hamza <hamzasarwari426@gmail.com>
2026-08-14 20:30:11 +0100

    prepared the page to have 2 controls for angles and 1 output

--- Website/script.js
+++ Website/script.js
@@ -1,9 +1,7 @@
 const panSlider = document.getElementById("panSlider");
-const panAngle = document.getElementById("panAngle");
 const panDisplay=document.getElementById("panDisplay");
 
 const tiltSlider = document.getElementById("tiltSlider");
-const tiltAngle = document.getElementById("tiltAngle");
 const tiltDisplay= document.getElementById("tiltDisplay");
 
 const homeButton = document.getElementById("homeButton");
@@ -13,45 +11,27 @@ const homeButton = document.getElementById("homeButton");
 panSlider.addEventListener("input", function() {
 
     const angle = panSlider.value;
-    panAngle.value = angle;
     panDisplay.textContent = angle;
 
 });
 
-// Pan Number Logic
-panAngle.addEventListener("input", function(){
-
-    const angle = panAngle.value;
-    panSlider.value = angle;
-    panDisplay.textContent = angle;
-});
 
 // Tilt Slider Logic
 tiltSlider.addEventListener("input", function(){
     
     angle = tiltSlider.value;
-    tiltAngle.value = angle;
     tiltDisplay.textContent = angle;
 
 });
 
-// Tilt Number Logic
-tiltAngle.addEventListener("input", function(){
-    
-    angle = tiltAngle.value;
-    tiltSlider.value = angle;
-    tiltDisplay.textContent = angle;
-
-});
 
 //Home Button Logic
 homeButton.addEventListener("click", function(){
 
-    panAngle.value = 90;
     panSlider.value = 90;
     panDisplay.textContent = 90;
 
-    tiltAngle.value = 90;
+
     tiltSlider.value = 90;
     tiltDisplay.textContent = 90;
 });
