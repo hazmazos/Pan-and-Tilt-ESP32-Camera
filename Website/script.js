fbcf3fa0f2b052d9bf3da182f39f57e0efef6f45
Hamza Sarwari <hamzasarwari426@gmail.com>
2026-08-15 22:54:03 +0100

    worked on getting co-ordinated from the map to convert to angles

--- Website/script.js
+++ Website/script.js
@@ -4,6 +4,9 @@ const panDisplay=document.getElementById("panDisplay");
 const tiltSlider = document.getElementById("tiltSlider");
 const tiltDisplay= document.getElementById("tiltDisplay");
 
+const mapInput = document.getElementById("gridXY");
+const rect = mapInput.getBoundingClientRect();
+
 const homeButton = document.getElementById("homeButton");
 
 
@@ -21,6 +24,16 @@ tiltSlider.addEventListener("input", function(){
     
     angle = tiltSlider.value;
     tiltDisplay.textContent = angle;
+
+});
+
+mapInput.addEventListener("click", function(event){
+
+    xCoords = Math.round(event.clientX - rect.left);
+    yCoords = Math.round(event.clientY - rect.top);
+
+    console.log(xCoords);
+    console.log(yCoords);
 
 });
 
