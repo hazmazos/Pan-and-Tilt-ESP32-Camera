0e36ead6e0cce9bbedb5dc45691cc583994691d3
Hamza <hamzasarwari426@gmail.com>
2026-08-17 16:41:01 +0100

    local web server running on ESP32

--- ESP32/src/main.cpp
+++ ESP32/src/main.cpp
@@ -1,15 +1,67 @@
 #include <Arduino.h>
-#include <ESP32Servo.h>
+#include <LittleFS.h>
+#include <Wifi.h>
+#include <Webserver.h>
+#include <ESPmDNS.h>
 
-Servo myservo;
-const int servoPin = 13;
+//#include <ESP32Servo.h>
+
+//Servo myservo;
+//const int servoPin = 13;
+
+const char* ssid = "";
+const char* password = "";
+
+WebServer server(80);
 
 void setup() {
-  myservo.attach(servoPin);
-  myservo.write(0);
+   
+  Serial.begin(115200);
+  
+  if(!LittleFS.begin()){
+    Serial.println("Little FS mount failed");
+    return;
+  }
+
+  Serial.println("Little FS mount worked");
+
+  WiFi.begin(ssid,password);
+  Serial.println("Connecting to Wifi");
+
+  while(WiFi.status() != WL_CONNECTED){
+    delay(500);
+    Serial.print(".");
+  }
+
+  Serial.println("WiFi connected");
+
+  Serial.print("Ip address:");
+  Serial.println(WiFi.localIP());
+
+  if(MDNS.begin("esp32cam")){
+    Serial.println("Open http://esp32cam.local");
+  }
+
+  server.on("/", HTTP_GET, [](){
+
+    File file = LittleFS.open("/index.html", "r");
+
+    if(!file){
+      Serial.println("File not found");
+      return;
+    }
+    server.streamFile(file,"text/html");
+    file.close();
+    
+  });
+
+  server.serveStatic("/", LittleFS, "/");
+
+  server.begin();
 }
   
 
 void loop() {
+  server.handleClient();
 }
 
