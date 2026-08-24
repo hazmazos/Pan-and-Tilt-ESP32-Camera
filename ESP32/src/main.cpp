f82a4aa03f9a885fb29c92fff0a133de8654b106
Hamza <hamzasarwari426@gmail.com>
2026-08-24 11:48:48 +0100

    integrating camera onto webserver

--- ESP32/src/main.cpp
+++ ESP32/src/main.cpp
@@ -4,13 +4,38 @@
 #include <WebServer.h>
 #include <ESPmDNS.h>
 
-#include <ESP32Servo.h>
+#include <esp_camera.h>
 
+//#include <ESP32Servo.h>
+
+#define PWDN -1
+#define RESET -1
+#define XCLK 15
+#define SIOD 4
+#define SIOC 5
+
+//image data
+#define Y9 16
+#define Y8 17
+#define Y7 18
+#define Y6 12
+#define Y5 10
+#define Y4 8
+#define Y3 9
+#define Y2 11
+
+//timing
+#define VSYNC 6
+#define HREF 7
+#define PCLK 13
+
+/*
 Servo panServo;
 Servo tiltServo; 
 
-const int panServoPin = 1;
-const int tiltServoPin = 2;
+#define panServoPin 1
+#define tiltServoPin 2
+*/
 
 const char* ssid = "";
 const char* password = "";
@@ -20,7 +45,58 @@ WebServer server(80);
 void setup() {
    
   Serial.begin(115200);
+
+
+  //Camera set up
+  if (psramFound()) {
+    Serial.println("PSRAM found");
+  }
+
+  else {
+    Serial.println("PSRAM NOT found");
+  }
+
+  camera_config_t config = {};
+
+  config.ledc_channel = LEDC_CHANNEL_0;
+  config.ledc_timer = LEDC_TIMER_0;
+
+  config.pin_d0 = Y2;
+  config.pin_d1 = Y3;
+  config.pin_d2 = Y4;
+  config.pin_d3 = Y5;
+  config.pin_d4 = Y6;
+  config.pin_d5 = Y7;
+  config.pin_d6 = Y8;
+  config.pin_d7 = Y9;
+
+  config.pin_xclk = XCLK;
+  config.pin_pclk = PCLK;
+  config.pin_vsync = VSYNC;
+  config.pin_href = HREF;
+
+  config.pin_sccb_sda = SIOD;
+  config.pin_sccb_scl = SIOC;
+
+  config.pin_pwdn = PWDN;
+  config.pin_reset = RESET;
+
+  config.xclk_freq_hz = 20000000;
+  config.pixel_format = PIXFORMAT_JPEG;
+  config.frame_size = FRAMESIZE_VGA;
+  config.jpeg_quality = 8;
+  config.fb_count = 1;
+
+  esp_err_t err = esp_camera_init(&config);
+
+  if (err != ESP_OK){
+    Serial.printf("Camera set up failed error 0x%x\n",err);
+    return;
+  }
+
+  Serial.println("Camera set up");
   
+  //Web server set up
   if(!LittleFS.begin()){
     Serial.println("Little FS mount failed");
     return;
@@ -95,7 +171,7 @@ void setup() {
     Serial.print("Pan angle is: " );
     Serial.println(panAngle);
 
-    panServo.write(panAngle);
+    //panServo.write(panAngle);
     
 
     
@@ -111,20 +187,37 @@ void setup() {
     Serial.print("Tilt angle is: " );
     Serial.println(tiltAngle);
 
-    tiltServo.write(tiltAngle);
+    //tiltServo.write(tiltAngle);
    
 
     
     server.send(200,"text/plain","Tilt angle is: "+value);
 
-  });  
+  });
+  
+  server.on("/capture", HTTP_GET, []{
+
+    camera_fb_t *fb = esp_camera_fb_get();
+
+    if( fb == NULL){
+      server.send(500, "text/plain", "Camera capture failed");
+      return;
+    }
+
+    server.send_P(200, "image/jpeg", (const char *)fb->buf, fb->len);
+    
+    esp_camera_fb_return(fb);
+
+  });
 
   server.begin();
 
+  /*
   panServo.attach(panServoPin);
   tiltServo.attach(tiltServoPin);  
   panServo.write(90);
   tiltServo.write(90);
+  */
 }
   
 
